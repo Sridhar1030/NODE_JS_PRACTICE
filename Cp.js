41622375cd064554419d9846d6b0037abe96287b
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-02-14 15:06:57 +0530

    .

---
+++ Cp.js
@@ -0,0 +1,7 @@
+const cp = require ('child_process')
+
+
+// cp.execSync('calc.exe')
+// cp.execSync('start chrome')
+
+console.log("output: " + cp.execSync(' node demo.js'));
--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
 function add(a, b) {
-        console.log("The addition is "+ (a+b))
+  console.log("The addition is " + (a + b));
 }
 
 function subtract(a, b) {
-        console.log("subtraction is " + (a-b))
+  console.log("subtraction is " + (a - b));
 }
 function multiply(a, b) {
-        console.log( "the multiplication is " + a*b)
+  console.log("the multiplication is " + a * b);
 }
 function divide(a, b) {
-        console.log("Division is " + a / b)
+  console.log("Division is " + a / b);
 }
 
 module.exports = {
-    add: add,
-    subtract : subtract,
-    multiply : multiply,
-    divide: divide
-}
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+};
